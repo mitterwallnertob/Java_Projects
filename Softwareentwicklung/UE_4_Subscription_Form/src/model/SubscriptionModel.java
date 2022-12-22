e51abd58df81362c01915c750850cd5d40116c52
Tobias Mitterwallner <72546145+mitterwallnertob@users.noreply.github.com>
2022-12-22 01:28:09 +0100

    Add files via upload

---
+++ Softwareentwicklung/UE_4_Subscription_Form/src/model/SubscriptionModel.java
@@ -0,0 +1,15 @@
+package model;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class SubscriptionModel {
+    private final StringProperty first_name = new SimpleStringProperty();
+    private final StringProperty last_name = new SimpleStringProperty();
+    private final BooleanProperty jfx_PropertiesAndBindings = new SimpleBooleanProperty();
+    private final BooleanProperty jfx_Concurrency = new SimpleBooleanProperty();
+    private final BooleanProperty j_MasterOfTheUniverse = new SimpleBooleanProperty();
+
+
+}
